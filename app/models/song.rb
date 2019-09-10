a6b82721f9d8bec4c8c010051a047712a6f57e00
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-09-10 01:10:04 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artists
   belongs_to :genres
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
