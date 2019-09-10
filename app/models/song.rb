fadc489d7ce8f7512b84cef92e6f028ddd91ba93
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-09-10 01:21:42 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
-  belongs_to :artists
-  belongs_to :genres
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
